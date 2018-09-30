9ec103dd0d3f9e7b9005d8826643f07ad14df703
Liyuan Zhang <31772719+Swallow666@users.noreply.github.com>
2018-09-29 22:30:08 -0400

    Update regfile.vhd
    lab2 vhdl code - register file

--- regfile.vhd
+++ regfile.vhd
@@ -1,1 +1,34 @@
--- 32*32 register file
+-- 32 x 32 register file
+-- two read ports, one write port with write enable
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+entity regfile is
+port( din : in std_logic_vector(31 downto 0);
+reset : in std_logic;
+clk : in std_logic;
+write : in std_logic;
+read_a : in std_logic_vector(4 downto 0);
+read_b : in std_logic_vector(4 downto 0);
+write_address : in std_logic_vector(4 downto 0);
+out_a : out std_logic_vector(31 downto 0);
+out_b : out std_logic_vector(31 downto 0));
+end regfile ;
+
+architecture behav of regfile is
+type registerfile is array(0 to 31) of std_logic_vector(31 downto 0);
+signal registers : registerfile := (others=>(others=>'0'));
+begin
+process(reset,clk)          -- write
+begin
+if (reset='1') then
+registers<=(others=>(others=>'0'));
+elsif (clk'event and clk='1') then
+if (write='1') then
+registers(conv_integer(write_address))<=din;
+end if;
+end if;
+end process;
+out_a<=registers(conv_integer(read_a));   -- read
+out_b<=registers(conv_integer(read_b));
+end behav;
