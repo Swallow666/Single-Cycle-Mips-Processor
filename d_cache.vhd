ec869882beb43c77b1758436fb29295dbcafdde2
Swallow666 <liyuan0314@outlook.com>
2018-10-29 09:21:04 -0400

    update icache dcache

--- d_cache.vhd
+++ d_cache.vhd
@@ -12,13 +12,18 @@ entity d_cache is
 end d_cache;
 
 architecture behav of d_cache is
+  type dcache is array(0 to 31) of std_logic_vector(31 downto 0);
+  signal datas: dcache:(others=>(others=>'0'));
   begin
     process(reset,clk)
     begin
       if (reset='1') then
+        datas<=(others=>(others=>'0'));
       elsif (clk'event and clk='1') then
       if (data_write='1') then
+        datas(conv_integer(a_in))<=din;
       end if;
       end if;
     end process;
+    d_out<=datas(conv_integer(a_in));
 end behav;
--- i_cache.vhd
+++ i_cache.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
 entity i_cache is
   port(
   a_in: in std_logic_vector(4 downto 0);
@@ -8,44 +9,8 @@ entity i_cache is
 end i_cache;
 
 architecture behav of i_cache is
+  type icache is array(0 to 31) of std_logic_vector(31 downto 0);
+  signal instructions: icache:(others=>(others=>'0'));
   begin
-    process(a_in)
-    begin
-      case a_in is
-        when "00000"=>
-        when "00001"=>
-        when "00010"=>
-        when "00011"=>
-        when "00100"=>
-        when "00101"=>
-        when "00110"=>
-        when "00111"=>
-        when "01000"=>
-        when "01001"=>
-        when "01010"=>
-        when "01011"=>
-        when "01100"=>
-        when "01101"=>
-        when "01110"=>
-        when "01111"=>
-        when "10000"=>
-        when "10001"=>
-        when "10010"=>
-        when "10011"=>
-        when "10100"=>
-        when "10101"=>
-        when "10110"=>
-        when "10111"=>
-        when "11000"=>
-        when "11001"=>
-        when "11010"=>
-        when "11011"=>
-        when "11100"=>
-        when "11101"=>
-        when "11110"=>
-        when "11111"=>
-        when others=>
-        null;
-      end case;
-    end process;
+    d_out<=instructions(conv_integer(a_in));
 end behav;
